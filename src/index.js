1beee38f7d45f8d08f1104b2b0d6f001d512049d
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-08-26 11:32:33 -0500

    Se elimina algunas funcionalidades donde se crea un nuevo archivo y se deja el DOM.render

--- src/index.js
+++ src/index.js
@@ -2,59 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { App } from "./App";
 
-class Formulario extends React.Component {
-    
-    constructor(props){
-        super(props);
-        this.nombre = React.createRef();
-        this.apellido= React.createRef();
-    }
-
-    eventoReferencia(evento){
-        window.alert(`${this.nombre.current.value} ${this.nombre.current.value}`);
-    }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="abs-center">
-          <div className="card card-container">
-            <form>
-              <div class="mb-3">
-                
-                <label for="nombre" class="form-label">
-                  Ingrese su nombre
-                </label>
-                <input
-                  type="text"
-                  ref={this.nombre}
-                  class="form-control"
-                  placeholder="Nombre"
-                  
-                />
-              </div>
-
-              <div class="mb-3">
-                <label for="apellido" class="form-label">
-                  Ingrese su apellido
-                </label>
-                <input
-                  type="text"
-                  ref={this.apellido}
-                  class="form-control"
-                  placeholder="Apellido"
-                />
-              </div>    
-                       
-              <button type="submit" class="btn btn-primary" onClick={this.eventoReferencia.bind(this)}>
-                Obtener mi IP
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-ReactDOM.render(<Formulario />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
