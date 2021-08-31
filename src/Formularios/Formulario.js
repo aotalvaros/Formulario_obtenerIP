996f452c77bec96b723dbf67f94bcb8142d9417a
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-08-30 22:37:52 -0500

    Se traen todos los componentes de APP y se cambia la validaciones de los input

--- src/Formularios/Formulario.js
+++ src/Formularios/Formulario.js
@@ -1,42 +1,115 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Modals } from "../CarpetaModals/Modals";
 
-export const Formulario=({nombre,apellido,actualizarCampos,validarFormulario})=>{
-    return(
-        <>
-        <form>
-            <img className="img-serponsive logo-img" 
-          src="https://sedeelectronica.antioquia.gov.co/info/antioquia_se/media/bloque2071.png"></img>
-          <div>
-            
-          </div>
-              <div className="mb-3">
-                <label htmlFor="nombre" className="form-label">
-                  Ingrese su nombre
-                </label>
-                <input
-                  type="text"
-                  value={nombre}
-                  className="form-control"
-                  name="nombre"
-                  onChange={actualizarCampos}
-                  required
-                />
-              </div>
+export const Formulario = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-              <div className="mb-3">
-                <label htmlFor="apellido" className="form-label">
-                  Ingrese su apellido
-                </label>
-                <input
-                  type="text"
-                  value={apellido}
-                  className="form-control"
-                  name="apellido"
-                  onChange={actualizarCampos}
-                  required
-                />
-              </div>
-            </form>
-        </>
-    )
-}
+  const [formulario, setFormulario] = useState({
+    nombre: "",
+    apellido: "",
+  });
+  const [ventana, setVentana] = useState({ model: false });
+
+  const [usuarioIP, enviarIpUsuario] = useState("");
+
+  function actualizarCampos(evento) {
+    setFormulario({ ...formulario, [evento.target.name]: evento.target.value });
+
+    if (evento.target.value === "") {
+      enviarIpUsuario(null);
+    }
+  }
+
+  const consumoApi=()=> {   
+      fetch("https://api.ipify.org/?format=json")
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          enviarIpUsuario(datos.ip);
+        });
+
+      abrirModal();
+  }
+
+  const abrirModal = () => {
+    setVentana({ model: !ventana.model });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(consumoApi)}>
+        <img
+          className="img-serponsive logo-img"
+          src="https://sedeelectronica.antioquia.gov.co/info/antioquia_se/media/bloque2071.png"
+        ></img>
+        <div></div>
+        <div className="mb-3">
+          <label htmlFor="nombre" className="form-label">
+            Nombre
+          </label>
+          <input
+            {...register("nombre", {
+              required: true,
+              maxLength: 20,
+              minLength: 1,
+              pattern: /^[A-Za-z]+$/i
+            }
+            )}
+            type="text"
+            value={formulario.nombre}
+            className="form-control"
+            name="nombre"
+            onChange={actualizarCampos}
+          />
+        </div>
+        {errors?.nombre?.type === "required" && <p>Ingrese su nombre</p>}
+        {errors?.nombre?.type === "pattern" && (
+        <p>Solo caracteres alfanumericos </p>
+      )}
+
+        <div className="mb-3">
+          <label htmlFor="apellido" className="form-label">
+            Apellido
+          </label>
+          <input
+            {...register("apellido", {
+              required: true,
+              maxLength: 20,
+              minLength: 1,
+              pattern: /^[A-Za-z]+$/i
+            })}
+            type="text"
+            value={formulario.apellido}
+            className="form-control"
+            name="apellido"
+            onChange={actualizarCampos}
+          />
+        </div>
+
+        {errors?.apellido?.type === "required" && <p>Ingrese su apellido</p>}
+        {errors?.apellido?.type === "pattern" && (
+        <p>Solo caracteres alfanumericos </p>
+      )}
+        
+        <button
+          type="submit"
+          className="btn btn-primary"   
+        >
+          Obtener mi IP
+        </button>
+      </form>
+
+      <Modals
+        nombre={formulario.nombre}
+        apellido={formulario.apellido}
+        estado={ventana.model}
+        cerrarModel={abrirModal}
+        IP={usuarioIP}
+      />
+    </>
+  );
+};
