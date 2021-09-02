ae340469b0c06cb017e596cc6f609d9cd363adb4
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-09-02 09:15:34 -0500

    Se refactoriza el codigo que esta en el archivo de Formulario.js

--- src/Formularios/Formulario.js
+++ src/Formularios/Formulario.js
@@ -1,134 +1,119 @@
 import React, { useState } from "react";
 import { Modals } from "../CarpetaModals/Modals";
-import { Form, Col } from "react-bootstrap";
+import { Form, Col,Button, Modal } from "react-bootstrap";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
 
 export const Formulario = () => {
+
   const [formulario, setFormulario] = useState({
     nombre: "",
     apellido: "",
   });
-  const [ventana, setVentana] = useState({ model: false, model2: false });
-  const [validated, setValidated] = useState(false);
+  const [ventana, setVentana] = useState({ 
+    model: false, 
+    model2: false });
   const [usuarioIP, enviarIpUsuario] = useState("");
   const [esValido, formularioEsValido] = useState({
-    nombre: false,
-    apellido: false,
+    nombreEsValido: false,
+    apellidoEsValido: false,
   });
+  const [esValidoSpinner,spinnerEsValido]=useState(false);
 
   function actualizarCampos(evento) {
     setFormulario({ ...formulario, [evento.target.name]: evento.target.value });
-
-    if (evento.target.value === "") {
-      enviarIpUsuario(null);
-    }
   }
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    const prevents = () => {
-      event.preventDefault();
-      event.stopPropagation();
-    };
-    const validaprevents = () => {
-      prevents();
-      formularioEsValido({
-        nombre: true,
-        apellido: true,
-      });
-      setValidated(false);
-    };
-
-    if (form.checkValidity() === false) {
-      prevents();
-    }
-
-    if (
-      expresiones.numero.test(formulario.nombre) ||
-      expresiones.numero.test(formulario.apellido)
-    ) {
-      validaprevents();
-    } else if (
-      expresiones.usuario.test(formulario.nombre) &&
-      expresiones.usuario.test(formulario.apellido)
-    ) {
-      prevents();
-      formularioEsValido({
-        nombre: false,
-        apellido: false,
-      });
-      setValidated(true);
-      consumoApi();
+  const handleSubmit = () => {
+    if (expresiones.letras.test(formulario.nombre)
+      && expresiones.letras.test(formulario.apellido)) {    
+      formularioEsValido({nombreEsValido:false,apellidoEsValido:false});
+      consumirApi();
     } else {
-      validaprevents();
-    }
+      formularioEsValido({nombreEsValido:true,apellidoEsValido:true});
+    }  
   };
   const expresiones = {
-    usuario: /^[A-Za-z]+$/i,
-    numero: /^[\s0-9]+$/u,
+    letras: /^[A-Za-z]+$/i,
+    patronIP: /^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})+$/, 
   };
 
-  const consumoApi = () => {
+  const consumirApi = ()=> {
     fetch("https://api.ipify.org/?format=json")
       .then((respuesta) => respuesta.json())
-      .then((datos) => {
-        enviarIpUsuario(datos.ip);
+      .then((datos) =>{
+        enviarIpUsuario(datos.ip);                
+      } 
+      )
+      .then(spinnerEsValido(true))
+      .then((respuesta) =>{ 
+        abrirModal();
+        spinnerEsValido(false);      
       })
-      .then((respuesta) => abrirModal())
-      .catch((erro) => abrirModal2());
+      .catch((erro) => {
+        abrirModal2();
+        spinnerEsValido(false);
+      });
+    
   };
 
   const abrirModal = () => {
     setVentana({ model: !ventana.model });
   };
   const abrirModal2 = () => {
-    setVentana({ model2: !ventana.model2 });
+    setVentana({ model2: !ventana.model2 });   
   };
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form noValidate validated={esValido.nombreEsValido }  onSubmit={handleSubmit}>
         <img
           className="img-serponsive logo-img" alt=""
           src="https://sedeelectronica.antioquia.gov.co/info/antioquia_se/media/bloque2071.png"
-        ></img>
+        >         
+        </img>
 
-        <Form.Group as={Col} controlId="validationCustom01">
+        <Form.Group as={Col} id={"cuerpo"}>
           <Form.Label>Nombre</Form.Label>
           <Form.Control
             required
             type="text"
-            value={formulario.nombre}
-            className="form-control"
+            value={formulario.nombre}            
             name="nombre"
-            onChange={actualizarCampos}
-            isInvalid={esValido.nombre}
+            placeholder="nombre"
+            onChange={actualizarCampos}           
           />
           <Form.Control.Feedback type="invalid">
             ingresa un nombre valido
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="validationCustom01">
+        <Form.Group as={Col} >
           <Form.Label>Apellido</Form.Label>
           <Form.Control
             required
             type="text"
             value={formulario.apellido}
-            className="form-control"
             name="apellido"
+            placeholder="apellido"
             onChange={actualizarCampos}
-            isInvalid={esValido.apellido}
           />
           <Form.Control.Feedback type="invalid">
             ingresa un apellido valido
           </Form.Control.Feedback>
-
-          <button type="submit" className="btn btn-primary ">
-            Obtener mi IP
-          </button>
         </Form.Group>
       </Form>
 
+          <Button  onClick={handleSubmit} className="text-center mb-2">
+            Obtener mi IP
+          </Button>
+
+          <Modal show={esValidoSpinner} ria-labelledby="contained-modal-title-vcenter"
+      centered>
+          <ClipLoader loading={true}  size={50} speedMultiplier={0.7} color={"#216101"} />
+          </Modal>
+        
       <Modals
         nombre={formulario.nombre}
         apellido={formulario.apellido}
